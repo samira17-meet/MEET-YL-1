f952cdfea23e13e6c15748ba0bb42b9e733f7ec5
samira <sosa.mousa@gmail.com>
2015-11-04 18:28:48 +0200

    blah

---
+++ lab1.py
@@ -0,0 +1,19 @@
+print("samira " + "+ and " + "+ grace ")
+num1 = 20
+print(num1)
+
+num2 = (num1/2)
+print(num2)
+list1 = [1,2,4]
+a=0
+for x in list1:
+	print(x)
+for x in list1:
+	print(x*2)
+for x in list1:
+    a=x+a
+    print(a)
+import turtle
+turtle.goto(100,100)
+turtle.goto forward()
+turtle.mainloop()
